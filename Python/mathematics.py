ff1cf31cc9a755c23e77d0926632ac20782c6f8e
Eufrausino <gustavo.medeiros@ufv.br>
2024-05-11 15:32:24 -0300

    subindo bibliotecas uteis

---
+++ Python/mathematics.py
@@ -0,0 +1,5 @@
+import math
+print(math.pi)
+print(math.sqrt(100))
+print(math.sin(45))
+print(math.log(10,100))
---
+++ Python/matrix.py
@@ -0,0 +1,5 @@
+import numpy as np
+matrix = np.array([[2,3,4], [1,7,0], [0,1,0]])
+print(matrix)
+print(matrix.shape)
+print(matrix[2][1])
